3d394b67b8781b9835ec13ae0b4adc8ffea86a9e
Yucongkoo <yucongkoo0214@gmail.com>
2023-10-08 16:51:19 +0800

    Fix styling errors
    Fixed styling errors to pass checkstyle

--- src/main/java/seedu/address/model/person/EmptyAddress.java
+++ src/main/java/seedu/address/model/person/EmptyAddress.java
@@ -1,6 +1,12 @@
 package seedu.address.model.person;
 
-public class EmptyAddress extends Address{
+/**
+ * Represents an empty address.
+ */
+public class EmptyAddress extends Address {
+    /**
+     * Constructs an empty address object.
+     */
     public EmptyAddress() {
         super("-");
     }
