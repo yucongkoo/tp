d31b8dfa5322763d4abda935a40bfd1ba08440f7
Yucongkoo <yucongkoo0214@gmail.com>
2023-10-30 15:14:37 +0800

    Disable selection in ListView
    Disabled selection in ListView

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -27,6 +27,7 @@ public class PersonListPanel extends UiPart<Region> {
         super(FXML);
         personListView.setItems(personList);
         personListView.setCellFactory(listView -> new PersonListViewCell());
+        personListView.setSelectionModel(null);
     }
 
     /**
