1316ec0e5f27a7424766f698ae7d98fe869ded57
songfangyl <e0960536@u.nus.edu>
2023-10-30 14:10:11 +0800

    Remove prefix when user using remark command

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -4,17 +4,22 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Remark;
 
+import java.util.logging.Logger;
+
 
 /**
  * Parses input arguments and creates a new RemarkCommand object
  */
 public class RemarkCommandParser implements Parser<RemarkCommand> {
+
+    private static final Logger logger = LogsCenter.getLogger(RemarkCommandParser.class);
 
     /**
      * Parses input and return the RemarkCommand object.
@@ -26,18 +31,29 @@ public class RemarkCommandParser implements Parser<RemarkCommand> {
     public RemarkCommand parse(String args) throws ParseException {
         requireNonNull(args);
 
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
-                PREFIX_REMARK);
+        logger.fine("RemarkCommandParser parsing...");
 
+        String trimmedArgs = args.trim();
+        String[] separatedArgs = trimmedArgs.split("\\s+", 2);
+
+        assert separatedArgs.length >= 1 : "separatedArgs length is smaller than 1";
         Index index;
+
         try {
-            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+            index = ParserUtil.parseIndex(separatedArgs[0]);
         } catch (IllegalValueException ive) {
+            logger.finer("Parsing failed due to invalid command format");
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     RemarkCommand.MESSAGE_USAGE), ive);
         }
 
-        Remark remark = ParserUtil.parseRemark(argMultimap.getValue(PREFIX_REMARK).orElse(""));
+        Remark remark;
+
+        if (separatedArgs.length == 1) {
+            remark = ParserUtil.parseRemark("");
+        } else {
+            remark = ParserUtil.parseRemark(separatedArgs[1]);
+        }
 
         return new RemarkCommand(index, remark);
     }
