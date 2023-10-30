d524d8a9ec4c23439b67a3154edee9175d2b0fe3
songfangyl <e0960536@u.nus.edu>
2023-10-30 14:32:31 +0800

    Modify wrong import format

--- src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
@@ -1,7 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.commands.CommandTestUtil.*;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_REMARK_DESC;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.model.person.Remark.MESSAGE_CONSTRAINTS;
@@ -10,10 +10,8 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.PriorityCommand;
 import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.model.person.Remark;
-import seedu.address.model.priority.Priority;
 public class RemarkCommandParserTest {
 
     private static final String MESSAGE_INVALID_FORMAT =
