ea78b54514cd8906bbe06e3d1b3da15cd9944fc3
songfangyl <e0960536@u.nus.edu>
2023-10-30 14:14:00 +0800

    Modify wrong import format

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -2,7 +2,8 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+
+import java.util.logging.Logger;
 
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.index.Index;
@@ -11,7 +12,6 @@ import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Remark;
 
-import java.util.logging.Logger;
 
 
 /**
